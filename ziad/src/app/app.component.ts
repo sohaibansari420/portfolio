2042960e8ea81fea8125b1111ebbf7b6019923f0
ZiadTariq <ziadtariq802@gmail.com>
2025-06-09 00:40:02 +0100

    Fixed some errors

--- ziad/src/app/app.component.ts
+++ ziad/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { LanguageService } from "src/app/services/language/language.service"
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'muffeez-portfolio';
+  title = 'ziad-portfolio';
 
   constructor(
     private titleService: Title,
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
 
     this.languageService.initLanguage()
 
-    this.titleService.setTitle("Muffeez Khalid | MEAN Stack Developer | MERN Stack Developer | Software Engineer");
+    this.titleService.setTitle("Ziad Bin Tariq | Software Engineer");
     this.metaService.addTags([
       { name: 'keywords', content: 'Frontend, MEAN Stack Developer , MERN Stack Development , Software Engineer, software, developer' },
       { name: 'description', content: 'As a software engineer with expertise in both MEAN and MERN stacks, I have a comprehensive understanding of full-stack web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular and React, as well as back-end technologies such as Node.js and Express.' },
