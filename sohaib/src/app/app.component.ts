e27b1b0099a6e8b4e85f47a237a48c6f5e6c27f1
sohaibansari420 <sohaibfaheem44@gmail.com>
2025-06-20 21:20:32 +0100

    Initial Commit

--- sohaib/src/app/app.component.ts
+++ sohaib/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { LanguageService } from "src/app/services/language/language.service"
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'ziad-portfolio';
+  title = 'sohaib-portfolio';
 
   constructor(
     private titleService: Title,
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
 
     this.languageService.initLanguage()
 
-    this.titleService.setTitle("Ziad Bin Tariq | Software Engineer");
+    this.titleService.setTitle("sohaib Bin Tariq | Software Engineer");
     this.metaService.addTags([
       { name: 'keywords', content: 'Frontend, MEAN Stack Developer , MERN Stack Development , Software Engineer, software, developer' },
       { name: 'description', content: 'As a software engineer with expertise in both MEAN and MERN stacks, I have a comprehensive understanding of full-stack web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular and React, as well as back-end technologies such as Node.js and Express.' },
